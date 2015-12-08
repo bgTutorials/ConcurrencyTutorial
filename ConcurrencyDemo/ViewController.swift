00108ff099237807af1f0861eee87408ea599d8a
Brian Glowe <brianglowe@gmail.com>
2015-12-08 16:49:35 -0500

    Updated didSelect method to NSOperation Queue

--- ConcurrencyDemo/ViewController.swift
+++ ConcurrencyDemo/ViewController.swift
@@ -47,40 +47,82 @@ class ViewController: UIViewController {
         
     
         let queue = NSOperationQueue()
-
+        let operation1 = NSBlockOperation(block: {
+            let img1 = Downloader.downloadImageWithURL(imageURLs[0])
+            NSOperationQueue.mainQueue().addOperationWithBlock({
+                self.imageView1.image = img1
+            })
+        })
+        operation1.completionBlock = {
+            print("Operation 1 completed")
+        }
+        queue.addOperation(operation1)
+        
+        let operation2 = NSBlockOperation(block: {
+            let img2 = Downloader.downloadImageWithURL(imageURLs[1])
+            NSOperationQueue.mainQueue().addOperationWithBlock({
+                self.imageView2.image = img2
+            })
+        })
+        operation2.completionBlock = {
+            print("Operation 2 completed")
+        }
+        queue.addOperation(operation2)
+        
+        let operation3 = NSBlockOperation(block: {
+            let img3 = Downloader.downloadImageWithURL(imageURLs[2])
+            NSOperationQueue.mainQueue().addOperationWithBlock({
+                self.imageView3.image = img3
+            })
+        })
+        operation3.completionBlock = {
+            print("Operation 3 completed")
+        }
+        queue.addOperation(operation3)
+        
+        let operation4 = NSBlockOperation(block: {
+            let img4 = Downloader.downloadImageWithURL(imageURLs[3])
+            NSOperationQueue.mainQueue().addOperationWithBlock({
+                self.imageView4.image = img4
+            })
+        })
+        operation4.completionBlock = {
+            print("Operation 4 completed")
+        }
+        queue.addOperation(operation4)
         
     // First section of Operations queue tutorial
 //        let queue = NSOperationQueue()
         
-        queue.addOperationWithBlock { () -> Void in
-                let img1 = Downloader.downloadImageWithURL(imageURLs[0])
-                
-                NSOperationQueue.mainQueue().addOperationWithBlock({
-                    self.imageView1.image = img1
-                })
-            }
-            queue.addOperationWithBlock { () -> Void in
-                let img2 = Downloader.downloadImageWithURL(imageURLs[1])
-                
-                NSOperationQueue.mainQueue().addOperationWithBlock({
-                    self.imageView2.image = img2
-                })
-            }
-            queue.addOperationWithBlock { () -> Void in
-                let img3 = Downloader.downloadImageWithURL(imageURLs[2])
-                
-                NSOperationQueue.mainQueue().addOperationWithBlock({
-                    self.imageView3.image = img3
-                })
-            }
-            queue.addOperationWithBlock { () -> Void in
-                let img4 = Downloader.downloadImageWithURL(imageURLs[3])
-                
-                NSOperationQueue.mainQueue().addOperationWithBlock({
-                    self.imageView4.image = img4
-                })
-        }
-        
+//        queue.addOperationWithBlock { () -> Void in
+//                let img1 = Downloader.downloadImageWithURL(imageURLs[0])
+//                
+//                NSOperationQueue.mainQueue().addOperationWithBlock({
+//                    self.imageView1.image = img1
+//                })
+//            }
+//            queue.addOperationWithBlock { () -> Void in
+//                let img2 = Downloader.downloadImageWithURL(imageURLs[1])
+//                
+//                NSOperationQueue.mainQueue().addOperationWithBlock({
+//                    self.imageView2.image = img2
+//                })
+//            }
+//            queue.addOperationWithBlock { () -> Void in
+//                let img3 = Downloader.downloadImageWithURL(imageURLs[2])
+//                
+//                NSOperationQueue.mainQueue().addOperationWithBlock({
+//                    self.imageView3.image = img3
+//                })
+//            }
+//            queue.addOperationWithBlock { () -> Void in
+//                let img4 = Downloader.downloadImageWithURL(imageURLs[3])
+//                
+//                NSOperationQueue.mainQueue().addOperationWithBlock({
+//                    self.imageView4.image = img4
+//                })
+//        }
+       ////// ********End of first section of operations queue*****//////////
         
 ////////////***********BEGINNING OF GCD Tutorial section ************////////////////////////////////////////////////
 //    // Code from "Using Serial Dispatch Queues"
